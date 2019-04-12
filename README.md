561c19723d5d6b09936baac53f679a30339d5046
jordan.werre <47542013+jwerre1@users.noreply.github.com>
2019-04-11 21:48:56 -0400

    readme complete

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # sequelized-knowledge-tracker
-A logger, utilizing Sequelize.js, that tracks what coding concepts have been mastered or need work.
+A logger, utilizing __Sequelize.js__, that tracks what coding concepts have been mastered or need work.
+
+---
+
+## Functionality
+
+Upon visiting the [website](https://secure-stream-17549.herokuapp.com/), the user sees a description of the website, two columns ("Needs Work" and "Known"), as well as an "Add a Topic" area.
+
+![main-page](/public/assets/img/imgOne.jpeg)
+
+As the instructions state, the website's purpose is to "keep track of what topics you have mastered and what ones need work. 
+
+"To add a topic that needs work, simply type it into the box [at the bottom of the screen] and click 'Add'. This will create a button in the appropriate column.
+
+![add-gif](/public/assets/img/gifOne.gif)
+
+"Once that topic has been mastered, click on the applicable button to toggle over to the "Known" category.
+
+![move-to-known](/public/assets/img/gifTwo.gif)
+
+"If you discover later that you did not know a topic as well as you thought you did... Simply click on it again and it will toggle back to the "Needs Work" category."
+
+![move-back](/public/assets/img/gifThree.gif)
+
+## Built With
+
+* [Sequelize.js](http://docs.sequelizejs.com/) - a promise-based Node.js ORM. 
+* [npm Express](https://www.npmjs.com/package/express) - web framework for Node.
+* [npm Express Handlebars](https://www.npmjs.com/package/express-handlebars) - a Handlebars view engine for Express. 
+* [npm MySQL](https://www.npmjs.com/package/mysql) - a MySQL Node module.
+* [Bootstrap](https://getbootstrap.com/) - a CSS framework.
+
+## Authors
+* **Jordan Werre**
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,39 +1,38 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-app.get("/", function (req, res) {
+    app.get("/", function (req, res) {
 
-    db.Knowledge.findAll({}).then(function(result) {
-        let object = {
-            knowledge: result
-        }
-        // console.log(result);
-        res.render("index", object);
+        db.Knowledge.findAll({}).then(function (result) {
+            let object = {
+                knowledge: result
+            }
+            res.render("index", object);
+        });
     });
-});
 
-app.post("/api/knowledge/", function (req, res) {
-    db.Knowledge.create({
-        name: req.body.name  
-      }).then(function(result) {
-        res.json({ id: result.insertId });
-      });
-});
-
-app.put("/api/knowledge/:id", function (req, res) {
-    db.Knowledge.update({
-        known: req.body.known
-    }, {
-        where: {
-            id: req.params.id
-        }
-    }).then(function(result) {
-        if (result.changedRows == 0) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
+    app.post("/api/knowledge/", function (req, res) {
+        db.Knowledge.create({
+            name: req.body.name
+        }).then(function (result) {
+            res.json({ id: result.insertId });
+        });
     });
-});
+
+    app.put("/api/knowledge/:id", function (req, res) {
+        db.Knowledge.update({
+            known: req.body.known
+        }, {
+                where: {
+                    id: req.params.id
+                }
+            }).then(function (result) {
+                if (result.changedRows == 0) {
+                    return res.status(404).end();
+                } else {
+                    res.status(200).end();
+                }
+            });
+    });
 }
